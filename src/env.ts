8ca3e220a45152351e85da7f22e1aff1b957961f
enimri <enimri1@gmail.com>
2024-08-22 01:59:10 +0300

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,5 +1,5 @@
 export const Env = {
-    API_HOST: process.env.REACT_APP_API_HOST || 'http://localhost.ivao.aero:8080/api',
+    API_HOST: process.env.REACT_APP_API_HOST || 'https://api.kronos.xm.ivao.aero/api',
     LOGO_LIGHT: process.env.REACT_APP_LOGO_LIGHT || 'https://assets.br.ivao.aero/logos/kronos_light.svg',
     LOGO_DARK: process.env.REACT_APP_LOGO_DARK || 'https://assets.br.ivao.aero/logos/kronos_dark.svg',
     LOGO_SIDEBAR_LIGHT: process.env.REACT_APP_LOGO_SIDEBAR_LIGHT || 'https://assets.br.ivao.aero/logos/symbol_white.svg',
